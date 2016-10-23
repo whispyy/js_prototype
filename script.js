b5470500a307c6d9a64e52f8010bf259a9d65dac
JF <jf-durand@outlook.fr>
2016-10-23 21:17:52 +0200

    added features

--- script.js
+++ script.js
@@ -4,19 +4,33 @@ JF.__proto__ = Etudiant;
 */
 
 /* creer un prototype façon propre*/
-var Etudiant = function(nom,prenom,annee){
+var Etudiant = function(nom,prenom,annee, notes){
 	this.nom = nom;
 	this.prenom = prenom;
 	this.annee = annee;
+	if (notes != undefined){
+		this.notes = notes
+	}
 }
 
 var JF = new Etudiant('Durand',"JF",5);
 
 Etudiant.prototype.affichage = function(){
 	return "Nom :"+this.nom+
-		   ", Prenom :"+this.prenom
+		   ", Prenom :"+this.prenom+
 		   "Annee :"+this.annee;
 }
 
 /* preuve qu'on a crée un prototype*/
 console.log(JF.__proto__ == Etudiant.prototype);
+
+
+Etudiant.prototype.moyenne = function(){
+	var sum = 0;
+	for (var i = 0; i < this.notes.length; i++) {
+		sum +=this.notes[i];
+	}
+	return sum/this.notes.length;
+}
+
+var JF2 = new Etudiant('Durand',"JF",5,[10,15]);
